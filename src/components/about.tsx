5cbc9acc009ffbfe2e758909e0f7235a0d8451ca
j-mendez <jeff@a11ywatch.com>
2022-06-21 08:08:22 -0400

    chore(about): fix about sentence continue

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -11,7 +11,7 @@ const About = () => {
             things. I specialize in security, devops, data intensive
             applications, and software engineering. If you are looking for an
             experienced software engineer to create a new application, write new
-            features into an existing application, or help you make your
+            features into an existing application, help you make your
             application better by creating tests or refactoring code then we
             should get in touch!
           </p>
@@ -19,7 +19,7 @@ const About = () => {
         <div>
           Contact me at
           <address>
-            <a href={"jeff@a11ywatch.com"}>jeff@a11ywatch.com</a>
+            <a href={"mailto:jeff@a11ywatch.com"}>jeff@a11ywatch.com</a>
           </address>
           <p>
             My github is{" "}
